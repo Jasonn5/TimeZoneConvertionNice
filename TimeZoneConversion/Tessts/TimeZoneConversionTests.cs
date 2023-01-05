1afb38212df4e4a0dc7cb586b583655f608c7371
Jasonn5 <jasson.n021@gmail.com>
2023-01-04 21:42:04 -0400

    Commit-Fix

---
+++ TimeZoneConversion/Tessts/TimeZoneConversionTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeZoneConversion;
+
+namespace Tessts
+{
+    [TestClass]
+    public class TimeZoneConversionTests
+    {
+        [TestMethod]
+        public void Convert_ConvertDateTimeToAnotherTimeZone() 
+        {
+            string sourceTimeZone = "GMT";
+            string destinationTimeZone = "EAT";
+            DateTime dateTimeSent = new DateTime(2023, 1, 5, 0, 21, 0);
+            DateTime dateTimeExpected = new DateTime(2023, 1, 5, 3, 21, 0);
+
+            DateTime result = TimeZoneHelper.ConvertDateTime(sourceTimeZone, destinationTimeZone, dateTimeSent);
+
+            Assert.AreEqual(dateTimeExpected, result);
+        }
+
+        [TestMethod]
+        public void DaylightTime_GetIfDaylightOrWinterTimer()
+        {
+            string timeZone = "EAT";
+            DateTime dateTimeSent = new DateTime(2023, 1, 5, 0, 21, 0);
+            string resultExpected = "Invierno";
+
+            string result = TimeZoneHelper.IsDaylightSavingTime(timeZone, dateTimeSent);
+
+            Assert.AreEqual(resultExpected, result);
+        }
+
+        [TestMethod]
+        public void DifferenceBetweenTimeZones_GetInHourDifferenceBetweenTimeZones()
+        {
+            string timeZoneOne = "GMT";
+            string timeZoneTwo = "EAT";
+            double resultExpected = 3.0;
+
+            double result = TimeZoneHelper.DifferenceBetweenTimeZones(timeZoneOne, timeZoneTwo);
+
+            Assert.AreEqual(resultExpected, result);
+        }
+
+        [TestMethod]
+        public void DateFormat_GetDateInAFormat()
+        {
+            DateTime dateTimeSent = new DateTime(2023, 1, 5, 0, 21, 0);
+            string resultExpected = "20230105";
+
+            string result = TimeZoneHelper.DateFormat(dateTimeSent);
+
+            Assert.AreEqual(resultExpected, result);
+        }
+
+        [TestMethod]
+        public void TimeFormat_GetTimeInAFormat()
+        {
+            DateTime dateTimeSent = new DateTime(2023, 1, 5, 10, 12, 0);
+            string resultExpected = "101200";
+
+            string result = TimeZoneHelper.TimeFormat(dateTimeSent);
+
+            Assert.AreEqual(resultExpected, result);
+        }
+    }
+}
---
+++ TimeZoneConversion/TimeZoneConversion/TimeZoneHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeZoneConversion
+{
+    public class TimeZoneHelper
+    {
+        public static DateTime ConvertDateTime(string sourceTimeZone, string destinationTimeZone, DateTime dateTime)
+        {
+            TimeZoneInfo resultSourceTimeZone = TimeZoneInfo.FindSystemTimeZoneById(sourceTimeZone);
+            TimeZoneInfo resultDestinationTimeZone = TimeZoneInfo.FindSystemTimeZoneById(destinationTimeZone);
+            DateTime destinationDateTime = TimeZoneInfo.ConvertTime(dateTime, resultSourceTimeZone, resultDestinationTimeZone);
+
+            return destinationDateTime;
+        }
+
+        public static string IsDaylightSavingTime(string timeZone, DateTime dateTime)
+        {
+            TimeZoneInfo resultTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+            bool isSavingTime = resultTimeZone.IsDaylightSavingTime(dateTime);
+            var result = isSavingTime ? "Verano" : "Invierno";
+
+            return result;
+        }
+
+        public static double DifferenceBetweenTimeZones(string sourceTimeZone, string destinationTimeZone) 
+        {
+            TimeZoneInfo resultSourceTimeZone = TimeZoneInfo.FindSystemTimeZoneById(sourceTimeZone);
+            TimeZoneInfo resultDestinationTimeZone = TimeZoneInfo.FindSystemTimeZoneById(destinationTimeZone);
+            double differenceInHours = (resultDestinationTimeZone.BaseUtcOffset - resultSourceTimeZone.BaseUtcOffset).TotalHours;
+
+            return differenceInHours;
+        }
+
+        public static string DateFormat(DateTime dateTime)
+        {
+            string convertedDate = dateTime.ToString("yyyyMMdd");
+
+            return convertedDate;
+        }
+
+        public static string TimeFormat(DateTime dateTime)
+        {
+            string convertedTime = dateTime.ToString("HHmmss");
+
+            return convertedTime;
+        }
+    }
+}
